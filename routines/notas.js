edbe505286fec6143ce640b19c2234d6ed8ddaed
MARCELLO CAVALCANTI DE ALBUQUERQUE <FR042456@catolica.ubec>
2020-02-11 21:52:24 -0300

    testando funcao

--- routines/notas.js
+++ routines/notas.js
@@ -1,4 +1,4 @@
-function calcularMedia(n1, n2, sch){
+function calcularMedia(n1, n2, sch, fn){
     var media;
     if(n1 !== null && n2 !== null){
         media = (n1+n2)/2;
@@ -12,7 +12,9 @@ function calcularMedia(n1, n2, sch){
     else if (n1 === null && n2 === null && sch !== null){
         media = sch/2;
     }
-
+    if (media >=3 && media <7  && n1 !== null && n2 !== null && sch === null && fn !== null){
+        media = (media+fn)/2
+    }
     return media;
 }
 
--- test/notas.spec.js
+++ test/notas.spec.js
@@ -8,5 +8,32 @@ describe("Calcular Média", ()=> {
             expect(Media.calcularMedia(7,9)).to.equal(8);
 
         })
+        it("1 Unid = 3; 2 Unid = 5; Média = 4;", () => {
+            expect(Media.calcularMedia(3,5)).to.equal(4);
+        })
+
     })
+    describe("Com segunda chamada", () => {
+        it("1 Unid = 7; 2 Unid = null; seg = 9; Média = 8;", () => {
+            expect(Media.calcularMedia(7, null, 9)).to.equal(8);
+        })
+        it("1 Unid = null; 2 Unid = 7; Média = 9;", () => {
+            expect(Media.calcularMedia(null, 7, 9)).to.equal(8);
+        })
+        it("1 Unid = null; 2 Unid = null; Média = 8;", () => {
+            expect(Media.calcularMedia(null, null, 8)).to.equal(4);
+        })
+    })
+    describe("Com final", () => {
+        it("1  media = 7; n1 = null ; n2 = null; fn = 9; media = 8;", () => {
+            expect(Media.calcularMedia(7, null, 9)).to.equal(8);
+        })
+        it("1  media = 4; n1 = 6 ; n2 = null; fn = 8; media = 6.5", () => {
+            expect(Media.calcularMedia(4, 6, null, 8)).to.equal(6.5);
+        })
+        it("1  media = 6; n1 = null ;  n2 = null ; fn = 8; media = 7", () => {
+            expect(Media.calcularMedia(6, null, 8)).to.equal(7);
+        })
+    })
+
 })
