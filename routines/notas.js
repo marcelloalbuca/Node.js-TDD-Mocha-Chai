808a8d08723b9ad200f986f8573d4ee002ae4ecf
MARCELLO CAVALCANTI DE ALBUQUERQUE <FR042456@catolica.ubec>
2020-02-11 21:08:18 -0300

    Funcao para teste calcular media

--- routines/notas.js
+++ routines/notas.js
@@ -3,4 +3,13 @@ function calcularMedia(n1, n2, sch){
     if(n1 !== null && n2 !== null){
         media = (n1+n2)/2;
     }
+    else if (n1 !== null && sch !== null){
+        media = (n1+sch)/2;
+    }
+    else if (n2 !== null && sch !== null){
+        media = (n2+sch)/2;
+    }
+    else if (n1 === null && n2 === null && sch !== null){
+        media = sch/2;
+    }
 }
